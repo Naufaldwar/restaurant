0f04ea80ff2fbe3641b2a516ebc29b1549cdb3de
Naufaldwar <naufaldwiariananto@gmail.com>
2023-05-13 14:51:39 +0700

    update linting

--- next.config.mjs
+++ next.config.mjs
@@ -18,9 +18,12 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-  // env: {
-  //   API_BASE_URL: process.env.API_BASE_URL || "",
-  //   NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
-  // },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL || "",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID || "",
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET || "",
+  },
 };
 export default config;
